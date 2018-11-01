270068ffd6d5181273e97740850fd3f75358984f
WIN-M0781SQ4NDE\Administrator <xxxlsu@naver.com>
2018-11-01 14:50:24 +0900

    크기조정

--- validate.js
+++ validate.js
@@ -313,19 +313,19 @@
 					if(i%3==0){
 						
 					createtable += "<tr><td><input type='text' class='inventory'  name='inventory["
-							+ i + "]' id='inventory["+ i + "]'></td><td><input type='text' class='mac_address'  name='mac_address["
-							+ i + "]' id='mac_address["+ i + "]' ></td>";
+							+ i + "]' id='inventory["+ i + "]'  style='margin-right: 10px; margin-bottom:5px;'></td><td><input type='text' class='mac_address'  name='mac_address["
+							+ i + "]' id='mac_address["+ i + "]'  style='margin-right: 10px; margin-bottom:5px;'></td>";
 					}
 					else if(i%3 != 0 && i % 3 != 2){
 						
 						createtable += "<td><input type='text' class='inventory'  name='inventory["
-							+ i + "]' id='inventory["+ i + "]'  ></td><td><input type='text' class='mac_address'  name='mac_address["
-							+ i + "]' id='mac_address["+ i + "]' ></td>";
+							+ i + "]' id='inventory["+ i + "]'  style='margin-right: 10px; margin-bottom:5px;'></td><td><input type='text' class='mac_address'  name='mac_address["
+							+ i + "]' id='mac_address["+ i + "]'  style='margin-right: 10px; margin-bottom:5px;'></td>";
 					}
 					else if (i % 3 == 2) {
 						createtable += "<td><input type='text' class='inventory'  name='inventory["
-							+ i + "]' id='inventory["+ i + "]' ></td><td><input type='text' class='mac_address'  name='mac_address["
-							+ i + "]' id='mac_address["+ i + "]' ></td></tr>";
+							+ i + "]' id='inventory["+ i + "]'  style='margin-right: 10px; margin-bottom:5px;'></td><td><input type='text' class='mac_address'  name='mac_address["
+							+ i + "]' id='mac_address["+ i + "]'  style='margin-right: 10px; margin-bottom:5px;'></td></tr>";
 						
 					}
 					
